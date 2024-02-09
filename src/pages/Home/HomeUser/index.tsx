99556aefb2219e96afba62dc25dc21a862373e23
ToavinaMaria <ToavinaMaria>
2024-02-09 21:44:31 +0300

    fix:index.tsx

--- src/pages/Home/HomeUser/index.tsx
+++ src/pages/Home/HomeUser/index.tsx
@@ -1,5 +1,10 @@
+import Carousel from './HomeCarousel/Carousel';
+
 const HomeUser = () => {
-  return <h1>Hello User</h1>;
+  // Données à fournir pour la prop 'data'
+  const data = [{ date: new Date(), price: 0, description: 'coucou' }];
+
+  return <Carousel data={data} />;
 };
 
 export default HomeUser;
