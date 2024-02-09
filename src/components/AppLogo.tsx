d1943b1470522ff0fa4d0b17a557449e58538932
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-09 23:46:51 +0300

    feat: pacifico font config

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -1,11 +1,13 @@
-import { AccountBox } from '@mui/icons-material';
 import { Typography } from '@mui/material';
 
 const AppLogo = () => {
   return (
-    <Typography variant="body1" color="primary" className="flex-all-center gap-2">
-      <AccountBox fontSize="large" />
-      <span className="font-bold font-Quicksand text-[2rem]">App logo</span>
+    <Typography
+      variant="body1"
+      color="primary"
+      className="flex-all-center gap-2"
+    >
+      <span className="font-bold font-Pacifico text-[2rem]">Starte-o</span>
     </Typography>
   );
 };
