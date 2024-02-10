bfb46f83cac218ae10f24c24fe4d36153dec81e3
HidDen-MaChinA <hei.franco.3@gmail.com>
2024-02-10 08:17:30 +0100

    feat: test getIdeas provider

--- src/api/Ideas.ts
+++ src/api/Ideas.ts
@@ -1,7 +1,6 @@
 import fetcher from "./fetcher";
 
-interface createIdeaType  {
-    "id": "string",
+export interface createIdeaType {
     "name": "string",
     "status": "OPEN",
     "description": "string",
@@ -9,51 +8,87 @@ interface createIdeaType  {
     "image": "string"
 }
 
-interface createCommentsIdea {
+export interface CommentsIdeaType {
     "id": "string",
+    "idea": {
+        "id": "string",
+        "name": "string",
+        "status": "OPEN",
+        "description": "string",
+        "founder": "person@gmail.com",
+        "image": "string",
+        "creationDatetime": "2024-02-10T06:59:13.696Z",
+        "updatedDatetime": "2024-02-10T06:59:13.696Z",
+        "currentFunds": 0
+    },
+    "content": "string",
+    "user": {
+        "id": "string",
+        "firstname": "string",
+        "lastname": "string",
+        "email": "string"
+    }
+}
+
+export interface IdeaType {
+    "id": "string",
+    "name": "string",
+    "status": "OPEN",
+    "description": "string",
+    "founder": "person@gmail.com",
+    "image": "string",
+    "creationDatetime": "2024-02-10T06:54:37.287Z",
+    "updatedDatetime": "2024-02-10T06:54:37.287Z",
+    "currentFunds": 0
+}
+
+export interface createCommentsIdea {
     "userId": "string",
     "content": "string"
 }
 
-export class Ideas{
-  static async createIdea(idea:createIdeaType) {
-    fetcher.put("/ideas",idea).then((value)=>{
-        return value.data
-    }).catch((e)=>{
-        throw e;
-    })
-  }
+export class IdeasProvider {
+    static async createIdea(idea: createIdeaType): Promise<IdeaType> {
+        return fetcher.put("/ideas", idea).then((value) => {
+            return value.data
+        }).catch((e) => {
+            throw e;
+        })
+    }
 
-  static async getIdeas(page:number, page_size:number) {
-    fetcher.get(`/ideas?page=${page}&page_size=${page_size}`).then((value) => {
-        return value.data;
-    }).catch((e) => {
-        return e;
-    })
-  }
+    static async getIdeas(page: number, page_size: number): Promise<[IdeaType]> {
+        return fetcher.get(`/ideas?page=${page}&page_size=${page_size}`).then((value) => {
+            return value.data;
+        }).catch((e) => {
+            return e;
+        })
+    }
 
-  static async getIdeaByIdeaId(idea_id:string) {
-    fetcher.get(`/ideas/${idea_id}`).then((value) => {
-        return value.data;
-    }).catch((e) => {
-        return e;
-    })
-  }
+    static async getIdeaByIdeaId(idea_id: string): Promise<IdeaType> {
+        return fetcher.get(`/ideas/${idea_id}`).then((value) => {
+            return value.data;
+        }).catch((e) => {
+            return e;
+        })
+    }
 
-  static async getCommentsIdeaByIdeaId(page:number, page_size:number, idea_id:string) {
-    fetcher.get(`/ideas/${idea_id}/comments?page=${page}&page_size=${page_size}`).then((value) => {
-        return value.data;
-    }).catch((e) => {
-        return e;
-    })
-  }
+    static async getCommentsIdeaByIdeaId(page: number, page_size: number, idea_id: string): Promise<CommentsIdeaType> {
+        return fetcher.get(`/ideas/${idea_id}/comments?page=${page}&page_size=${page_size}`).then((value) => {
+            return value.data;
+        }).catch((e) => {
+            return e;
+        })
+    }
 
-  static async createCommentsIdeaByIdeaId(idea_id:string) {
-    fetcher.get(`/ideas/${idea_id}/comments`).then((value) => {
-        return value.data;
-    }).catch((e) => {
-        return e;
-    })
-  }
-  
+    static async createCommentsIdeaByIdeaId(idea_id: string, user_id: string, content: string) : Promise<CommentsIdeaType>{
+        return fetcher.put(`/ideas/${idea_id}/comments`,{
+            "userId": user_id,
+            "content": content
+        }).then((value) => {
+            return value.data;
+        }).catch((e) => {
+            return e;
+        })
+    }
+
 }
--- src/pages/Home/HomeUser/index.tsx
+++ src/pages/Home/HomeUser/index.tsx
@@ -1,8 +1,18 @@
+import { IdeaType, IdeasProvider } from '@/api/Ideas';
 import MenuAppBar from '@/components/UserNavBar/AppBar';
 import TemporaryDrawer from '@/components/UserNavBar/Drawer';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const HomeUser = (): React.ReactElement => {
+  const [Ideas, setIdeas] = useState<[IdeaType] | null>(null);
+  useEffect(()=>{
+    IdeasProvider.getIdeas(0,10).then((ideas:[IdeaType])=>{
+      setIdeas(ideas)
+      console.log(ideas)
+    }).catch((e)=>{
+      console.log(e)
+    });
+  },[])
   return (
     <>
       <MenuAppBar Drawer={TemporaryDrawer} />
