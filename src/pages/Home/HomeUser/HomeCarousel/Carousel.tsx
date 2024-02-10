ab114a3fb73f988d1ad0aad5c0586e2434b836eb
ToavinaMaria <ToavinaMaria>
2024-02-10 03:27:57 +0300

    fix: Carousel

--- src/pages/Home/HomeUser/HomeCarousel/Carousel.tsx
+++ src/pages/Home/HomeUser/HomeCarousel/Carousel.tsx
@@ -8,15 +8,11 @@ type HomeProps = {
 
 const Carousel: FC<HomeProps> = ({ data }) => {
   return (
-    <div className="full-view">
-      <Box marginTop="1rem" padding="1rem">
-        <Box className="flex justify-center flex-wrap gap-14">
-          {data.map((x) => (
-            <CardProfil item={x as ItemData} />
-          ))}
-        </Box>
-      </Box>
-    </div>
+    <Box className="flex justify-center flex-wrap">
+      {data.map((x) => (
+        <CardProfil item={x as ItemData} />
+      ))}
+    </Box>
   );
 };
 
