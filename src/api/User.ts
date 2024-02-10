bf9668032925710cab346d97dc49bf2271d0eab7
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-10 10:00:56 +0300

    refactor: remove whoami

--- src/api/User.ts
+++ src/api/User.ts
@@ -3,16 +3,6 @@ import { User as UserData } from '@/types/User.type';
 import Token from '@/core/token';
 
 class User {
-  public static async whoami(): Promise<UserData> {
-    return (
-      await fetcher.get('/auth/whoami', {
-        headers: {
-          Authorization: 'Bearer ' + Token.get(),
-        },
-      })
-    ).data;
-  }
-
   public static async setProfile(userId: string, data: any): Promise<UserData> {
     return (
       await fetcher.post(`/users/${userId}/picture/raw`, data, {
