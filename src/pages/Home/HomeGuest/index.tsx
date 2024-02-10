7b620d5a036a31a65d848c44b0190c5efde418f3
HidDen-MaChinA <hei.franco.3@gmail.com>
2024-02-10 05:01:03 +0100

    feat: ongoing guest home

--- src/pages/Home/HomeGuest/index.tsx
+++ src/pages/Home/HomeGuest/index.tsx
@@ -32,23 +32,6 @@ const HomeGuest = () => {
           </div>
         </span>
       </section>
-      <section className="py-[4rem] flex flex-row">
-        <span className="w-full">
-          <div className="w-full h-[400px] justify-center flex flex-row">
-            <div className="h-full w-1/2 p-7 relative overflow-hidden">
-              <div className="w-full">
-                <p className="text-2xl flex-start p-4 flex-wrap flex">
-                  People make really big effort on theire startup, this
-                  application is a walkthrough to help those little ideas on
-                  their journey to the top of the world
-                </p>
-                <p className="w-4/5 p-4 flex-wrap flex"></p>
-              </div>
-            </div>
-          </div>
-        </span>
-      </section>
-      <section className="bg-black h-[400px]"></section>
       <Footer />
     </div>
   );
