cf497c1b72ffd52fecc9af2ed440b859b2ead2cf
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-10 13:49:42 +0300

    feat: split home pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import pageRoutes from './@pageRoutes';
-import { SecureRoute } from '@/layer';
 import { PageNotFound, Ping } from '@/layout';
-import Home from './Home';
 import Profile from './Profile';
-import { Login, Signup, ForgotPassword } from './auth';
 import ViewIdea from '@/pages/ViewIdea';
+import HomeGuest from "@/pages/Home/HomeGuest";
+import HomeUser from "@/pages/Home/HomeUser";
+import { Login, Signup, ForgotPassword } from './auth';
 
 const Pages = () => {
   return (
     <Router>
       <Routes>
-        {/* Client pages */}
-        <Route path={pageRoutes.home} Component={Home} />
+        <Route path={pageRoutes.home} Component={HomeGuest} />
+        <Route path={pageRoutes.homeUser} Component={HomeUser} />
+
         <Route path={pageRoutes.profile} Component={Profile} />
         <Route path={pageRoutes.viewIdea} Component={ViewIdea} />
 
-        {/* Security example only using secureRoute */}
-        <Route
-          path="/admin"
-          Component={SecureRoute(
-            () => (
-              <p>Hello admin</p>
-            ),
-            ['admin'],
-          )}
-        />
-
-        {/* Auth pages */}
         <Route path={pageRoutes.login} Component={Login} />
         <Route path={pageRoutes.signup} Component={Signup} />
         <Route path={pageRoutes.forgotPassword} Component={ForgotPassword} />
 
-        {/* Unexpected pages */}
         <Route path="/ping" Component={Ping} />
         <Route path="*" Component={PageNotFound} />
       </Routes>
