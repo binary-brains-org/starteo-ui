c0c798188c11c62d4fdd7eb2dd2e3f189a7c5797
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-29 13:24:59 +0300

    feat: colors to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,26 @@
-import { colors, MakeFamily } from '@/assets';
+import { Config } from 'tailwindcss';
+import { colors, MakeFamily } from './src/assets';
 
 const pathSrc = './src/**/*.{html,js,jsx,ts,tsx,css}';
 
-/** @type {import('tailwindcss').Config} */
-const config = {
+const config: Config = {
   content: [pathSrc],
   darkMode: 'class',
   theme: {
     extend: {
       colors,
-      fontFamily: MakeFamily,
+      fontFamily: MakeFamily as any,
       animation: {
         fade: 'fade-show linear 0.5s 1 0.1s normal forwards',
       },
       keyframes: {
         'fade-show': {
           from: {
-            opacity: 0,
+            opacity: '0',
             transform: 'scale(1.5) translateY(1rem)',
           },
           to: {
-            opacity: 1,
+            opacity: '1',
             transform: 'scale(1) translateY(0)',
           },
         },
