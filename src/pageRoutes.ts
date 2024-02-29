8c88ca4a0480e02d96b4eb8bd48bdda2366a5248
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-29 13:39:32 +0300

    feat: page routes

--- src/pageRoutes.ts
+++ src/pageRoutes.ts
@@ -2,6 +2,9 @@ import { Parameterized } from '@/utils';
 
 const pageRoutes = {
   home: '/',
+  project: '/project',
+  funding: '/funding',
+  payments: '/payments',
   profile: new Parameterized('/profile/:uuid'),
   signup: '/signup',
   login: '/login',
